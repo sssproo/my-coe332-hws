8740e08ef443312bba9e95cddd0010313694a69a
sssproo <111920654+sssproo@users.noreply.github.com>
2023-01-26 14:22:00 -0600

    Update script01.py

--- homework01/script01.py
+++ homework01/script01.py
@@ -1,8 +1,10 @@
-words = []
+word = []
+sorted_words =[]
+
 with open('words','r') as f:
-    contents = f.readlines()
-    for i in range(len(contents)):
-        words.append(contents[i].strip('\n'))
-words.sort(key=lambda x: (len(x), x), reverse=True)
-for i in range(5):
-  print(words[:5])
+
+    words =f.read().splitlines()
+
+sorted_words =words.sort(key=len,reverse=True)
+
+print(words[:5])
