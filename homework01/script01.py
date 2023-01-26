06aac619090541a7ae9dec10cb4e1bc30d01f100
sssproo <111920654+sssproo@users.noreply.github.com>
2023-01-26 14:13:36 -0600

    Update script01.py

--- homework01/script01.py
+++ homework01/script01.py
@@ -1,7 +1,8 @@
 words = []
-with open('/usr/share/dict/words','r') as f:
+with open('words','r') as f:
     contents = f.readlines()
     for i in range(len(contents)):
         words.append(contents[i].strip('\n'))
 words.sort(key=lambda x: (len(x), x), reverse=True)
-print(words)
+for i in range(5):
+  print(words[:5])
