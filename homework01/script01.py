542e9e2f8cec326104a4df6002fb4d9bf7451179
sssproo <111920654+sssproo@users.noreply.github.com>
2023-01-26 10:59:33 -0600

    Update script01.py

--- homework01/script01.py
+++ homework01/script01.py
@@ -1,6 +1,6 @@
 words = []
-with open('words.txt', encoding='utf-8') as file:
-    contents = file.readlines()
+with open('/usr/share/dict/words','r') as f:
+    contents = f.readlines()
     for i in range(len(contents)):
         words.append(contents[i].strip('\n'))
 words.sort(key=lambda x: (len(x), x), reverse=True)
