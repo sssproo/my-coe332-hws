40cf444b064cd27fcaa8174d916699bb84fa1081
sssproo <111920654+sssproo@users.noreply.github.com>
2023-01-26 10:42:37 -0600

    Update script03.py

--- homework01/script03.py
+++ homework01/script03.py
@@ -1,1 +1,22 @@
+import names
+
+
+def calculate(name_list):
+    for name in name_list:
+        print(name, len(name) - 1)
+
+
+def main():
+    name_list = []
+    cnt = 0
+    while cnt < 5:
+        name = names.get_full_name()
+        if name not in name_list:
+            name_list.append(name)
+            cnt += 1
+    calculate(name_list)
+
+
+if __name__ == '__main__':
+    main()
 
