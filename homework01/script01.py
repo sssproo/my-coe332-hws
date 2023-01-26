a2555df2859e58822917a750bab9e76827768e74
sssproo <111920654+sssproo@users.noreply.github.com>
2023-01-26 10:40:34 -0600

    Update script01.py

--- homework01/script01.py
+++ homework01/script01.py
@@ -1,1 +1,7 @@
-
+words = []
+with open('words.txt', encoding='utf-8') as file:
+    contents = file.readlines()
+    for i in range(len(contents)):
+        words.append(contents[i].strip('\n'))
+words.sort(key=lambda x: (len(x), x), reverse=True)
+print(words)
