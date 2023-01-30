a9c92083c31859f8a2ceb4e3fc5ad228bac58876
sssproo <111920654+sssproo@users.noreply.github.com>
2023-01-30 09:30:53 -0600

    Update generate_sites.py

--- homework02/generate_sites.py
+++ homework02/generate_sites.py
@@ -1,1 +1,20 @@
+import random
+import json
 
+
+def main():
+    n = 5
+    s = "sites"
+    dic = {s: []}
+    compositions = ["stony", "iron", "stony-iron"]
+    for i in range(n):
+        latitude = random.uniform(16.0, 18.0)
+        longitudes = random.uniform(82.0, 84.0)
+        composition = compositions[random.randint(0, 2)]
+        dic[s].append({'site_id': i + 1, 'latitude': latitude, 'longitude': longitudes, 'composition': composition})
+    with open("sites.json", 'w') as write_f:
+        json.dump(dic, write_f, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main()
